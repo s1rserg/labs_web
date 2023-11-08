c3fee40d588bbc6ac69dd4779409acda640cef25
s1rserg <87946455+s1rserg@users.noreply.github.com>
2023-11-08 16:32:38 +0200

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -72,6 +72,7 @@ window.onload = function () {
 
   if (localStorage.getItem("div3Alignment") != null) {
     div3.style.textAlign = localStorage.getItem("div3Alignment");
+    div3.style.alignItems = "start";
   }
   if (localStorage.getItem("div4Alignment") != null) {
     div4.style.textAlign = localStorage.getItem("div4Alignment");
